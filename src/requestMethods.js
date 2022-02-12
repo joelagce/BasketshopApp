a2d3578e5f28081d4c6d8267ba41fa3ea57428cf
joelagce <joelagundez17@gmail.com>
2022-02-12 11:24:03 -0700

    hola

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:2000/api/";
+const BASE_URL = "http://3.22.171.189/api/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
